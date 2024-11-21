ce357660c35e7988420664b323b7e2a896cf3122
Keysaib <144979172+Keysaib@users.noreply.github.com>
2024-11-21 13:36:19 -0500

    Update run.py

--- flask_project/run.py
+++ flask_project/run.py
@@ -3,4 +3,4 @@ from website import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5001)  # Change 5001 to any other available port
+    app.run(debug=True, host='0.0.0.0')  # Change 5001 to any other available port
